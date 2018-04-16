2268cdc022bdbcb973d00806471148fba36bbc84
florm <florenciamartin05@gmail.com>
2018-04-16 13:54:58 -0300

    se agregan las clases de modelo barrio y direccion

---
+++ Farmacia/src/main/java/ar/edu/unlam/tallerweb1/modelo/Barrio.java
@@ -0,0 +1,85 @@
+package ar.edu.unlam.tallerweb1.modelo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Barrio {
+
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nombre;
+	
+	@ManyToOne
+	private Comuna comuna;
+
+	//constructores
+	public Barrio(){}
+	
+	public Barrio (String nombre){
+		this.nombre = nombre;
+	}
+	
+	//getters y setters
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Comuna getComuna() {
+		return comuna;
+	}
+
+	public void setComuna(Comuna comuna) {
+		this.comuna = comuna;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+		return result;
+	}
+
+	//hashcode e equals
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Barrio other = (Barrio) obj;
+		if (nombre == null) {
+			if (other.nombre != null)
+				return false;
+		} else if (!nombre.equals(other.nombre))
+			return false;
+		return true;
+	}
+
+	
+	
+	
+	
+	
+}
